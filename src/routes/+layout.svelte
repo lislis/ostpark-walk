10bd6d8b956eb8145467a3657ebc2fb66f296b3c
lislis <mail@lislis.de>
2024-10-03 19:43:59 +0200

    add new pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/hilfe">Hilfe</a>
+	<a href="/pois">POIs</a>
+</nav>
+
+<slot></slot>
--- src/routes/poi/[id]/+page.svelte
+++ src/routes/poi/[id]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    export let id;
+ import data from '$lib/data/data.json';
+ import { page } from '$app/stores';
 
+ const poi = data.pois.find(x => x.id == $page.params.id)
 </script>
 
-<h1>POI</h1>
 
-<p>{id}</p>
+<h1>POI {poi.id}</h1>
+
+{poi.type}
