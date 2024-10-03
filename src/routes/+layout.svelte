4dd311cf5ab85b9cb6d7beb70994816dc11a0e2f
lislis <mail@lislis.de>
2024-10-03 21:44:36 +0200

    base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
+<script>
+ import '../app.css'
+
+ let src = "spp.png";
+</script>
+
+<header>
+<h1 class="logo"><img {src} width="130" alt="sterna | pau produktionen" /></h1>
+
 <nav>
-	<a href="/">Home</a>
-	<a href="/hilfe">Hilfe</a>
-	<a href="/pois">POIs</a>
+    <a href="/">Home</a>
+    <a href="/hilfe">Hilfe</a>
+    <a href="/pois">POIs</a>
 </nav>
+</header>
 
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Ostpark</h1>
+<h1>Willkommen zur Ostpark-Experience!</h1>
+
+<p>Lorem ipsum dolor sit amet!</p>
+
+<a href="/karte" class="button">Los geht's!</a>
