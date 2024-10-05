9b7be358082973f2fa64845ccc441d21639c3ffc
lislis <mail@lislis.de>
2024-10-05 21:09:39 +0200

    try different poit

--- src/lib/components/poi_ar.svelte
+++ src/lib/components/poi_ar.svelte
@@ -7,6 +7,6 @@
 <div class="aframe">
 <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
     <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-    <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 50.133914; longitude: 11.430639" scale="10 10 10"></a-entity>
+    <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 50.134211; longitude: 11.430251" scale="10 10 10"></a-entity>
 </a-scene>
 </div>
