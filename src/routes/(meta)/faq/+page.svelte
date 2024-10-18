3ee66c54b1b20a1cdecde9d17c7fe171059a74a9
jolandauhlig <54583851+jolandaschmidtt@users.noreply.github.com>
2024-10-18 11:49:25 +0200

    Update +page.svelte
    added texts from Figma

--- src/routes/(meta)/faq/+page.svelte
+++ src/routes/(meta)/faq/+page.svelte
@@ -1,6 +1,46 @@
 <h1 class="a11y-hidden">FAQ</h1>
 
 <p>
-<strong>Frage</strong><br>
-Antwort
+<strong>Ich kann mich selbst nicht auf der Karte sehen.</strong><br>
+Wenn die App keine Berechtigung hat, dein Smartphone zu orten, weiß sie nicht, wo du bist. 
+Wenn die App dich nach der Berechtigung fragt, auf deinen Standort zuzugreifen, klicke auf “Zulassen”. 
+Deine Daten werden nicht mit Dritten geteilt und nur für die Dauer der Nutzung der App gespeichert.
+Falls dies nicht funktioniert, schaue in den Einstellungen deines Smartphones nach, 
+ob du die Berechtigung gegeben hast.
 </p>
+
+<p>
+<strong>Ich kann nichts hören.</strong><br>
+Befindest du dich in der Nähe eines der Audio-Symbole? Es sollte aufleuchten, wenn du nah genug bist. 
+Ist deine Handylautstärke hoch genug? 
+Sind deine Kopfhörer korrekt mit deinem Handy verbunden?
+</p>
+
+<p>
+<strong>Ich kann keine Medien auf der Karte sehen.</strong><br>
+Für die Benutzung der App musst du dich auf dem Ostfriedhof in Dortmund aufhalten. 
+Falls du heute nicht in der Nähe bist, komm doch gern ein andermal vorbei!
+</p>
+
+<p>
+<strong>Die Kamera-Ansicht funktioniert bei mir nicht.</strong><br>
+Hast du die Berechtigung gegeben, auf deine Kamera zuzugreifen? 
+Wenn nicht, hole dies in den Einstellungen deines Smartphones nach. 
+Falls du das schon getan hast, wissen wir leider auch nicht weiter... 
+Auf manchen Handys funktioniert diese Technik leider nicht, sorry!
+</p>
+
+<p>
+
+<strong>Die Kamera-Ansicht funktioniert, aber ich kann nichts sehen außer dem Kamerabild...</strong><br>
+Hm, hast du dich gut umgeschaut und in allen Richtungen nachgeguckt? 
+Bist du im Umkreis von 10 Metern eines Ortes mit dem AR-Zeichen? 
+Falls ja, wissen wir leider auch nicht weiter... 
+</p>
+
+<p>
+
+<strong></strong><br>
+</p>
+
+
