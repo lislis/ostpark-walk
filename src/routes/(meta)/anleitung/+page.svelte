a656b76820a6d4368845d1387f1d692730771784
jolandauhlig <54583851+jolandaschmidtt@users.noreply.github.com>
2024-10-18 11:58:25 +0200

    Update +page.svelte
    added texts from figma

--- src/routes/(meta)/anleitung/+page.svelte
+++ src/routes/(meta)/anleitung/+page.svelte
@@ -1,18 +1,45 @@
 <h1 class="a11y-hidden">Anleitung</h1>
 
 <div class="gridlike">
-    <h2>GPS</h2>
+    <h2>Karte</h2>
     <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac erat est. Donec pretium condimentum ex, sed cursus ligula porttitor et. Praesent tempus auctor porttitor. </p>
+        <p>
+        In der Karten-Ansicht kannst du sehen, wo du dich gerade befindest und an welchen Orten sich verschiedene Medien verstecken. 
+        <strong>Wichtig:</strong> Bitte gib der App die Berechtigung, auf deinen Standort zuzugreifen!
+        </p>
     </div>
 
-    <h2>Kamera</h2>
+    <h2>GPS</h2>
     <div>
-        <p>In felis tortor, ullamcorper at magna ut, volutpat congue diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <p>
+        Im Zentrum der Karte siehst du deine Position auf der Karte. 
+        </p>
+    </div>
+
+    <h2>3D</h2>
+    <div>
+        <p>
+        Das Rechteck steht für 3D-Elemente, die du durch deine Smartphone-Kamera sehen kannst. 
+        Wenn du in der Nähe des markierten Ortes bist, schalte auf die Kamera-Ansicht um. 
+        <strong>Wichtig: </strong> Bitte gib der App die Berechtigung, auf deine Kamera zuzugreifen! 
+        </p>
     </div>
 
     <h2>Audio</h2>
     <div>
-        <p>Lorem ipsum</p>
+        <p>
+        Das Lautsprecher-Symbol steht für Medien, die man hören kann. 
+        Sie fangen automatisch an zu spielen, wenn du in einem Umkreis von 10 Meter des markierten Ortes bist.
+        </p>
     </div>
+
+
+    <h2>Kamera</h2>
+    <div>
+        <p>
+        In der Kamera-Ansicht kannst du die Welt um dich herum durch die Kamera deines Smartphones sehen. 
+        Hier haben sich einige Dinge versteckt, die du mit deinen eigenen Augen nicht wahrnehmen kannst.     
+        </p>
+    </div>
+
 </div>
