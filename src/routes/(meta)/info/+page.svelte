f7fdce8d742de14a141a5e39441c5179be68f988
jolandauhlig <54583851+jolandaschmidtt@users.noreply.github.com>
2024-10-18 12:22:08 +0200

    Update +page.svelte
    added texts from figma + website

--- src/routes/(meta)/info/+page.svelte
+++ src/routes/(meta)/info/+page.svelte
@@ -2,5 +2,61 @@
 
 <p>
 <strong>Worum geht's?</strong><br>
-Antwort
+Was ist eigentlich Zeit? Ist sie relativ? Und wenn ja, zu was? Kann man sie verlieren? 
+Warum ist sie ständig so knapp? Fehlt dir die Zeit, eine Pause zu machen? 
+Spielt es keine Rolle, weil die Geschichte sich wiederholt? 
+Ist es schon fünf vor zwölf? <br>
+Jetzt nur keine Zeit verlieren – jetzt alle Zeit der Welt verlieren. 
+Take your time, nimm dir die Zeit zurück, in deinem ganz eigenen Tempo.<br>
+STERNA | PAU haben für dich den Ostfriedhof erkundet, Interviews mit Spaziergänger:innen, Friedhofsgärtner:innen, 
+Historiker:innen und Bestatter:innen geführt und sich von Eichhörnchen und Bronzestatuen inspirieren lassen.
+„VON ANDEREN ZEITEN“ ist ein interaktiver Spaziergang, der dich mithilfe deines Smartphones über den Ostfriedhof führt. 
+Das ganze Jahr, in deiner eigenen Geschwindigkeit. 
+Das Projekt lädt dazu ein, sich Zeit zu nehmen, eine Pause zu machen und Orte der Entspannung mitten in der Großstadt zu entdecken.
 </p>
+
+<p>
+<strong>Termine</strong><br>
+Eröffnungswalks mit performativen Elementen:
+Sonntag, 27.10.2024, 16 Uhr <br>
+Samstag, 02.11.2024, 16 Uhr <br>
+Ostfriedhof, Dortmund <br>
+Treffpunkt Haupteingang <br>
+</p>
+
+<p>
+<strong>Über uns</strong><br>
+STERNA │ PAU Produktionen ist ein Theater- und Performance-Netzwerk aus Bochum, Berlin und Dortmund. 
+Die Künstler*innen produzieren Theater für Jugendliche und Erwachsene und erproben dabei immer 
+wieder neue Formen von Teilhabe  –  im Entstehungsprozess sowie in Aufführungssituationen. 
+Gerne arbeiten sie auch mit nicht-menschlichen Akteur*innen und transferieren Theater in digitale Räume. 
+Im Mittelpunkt stehen vor allem Fragen des Zusammenlebens, Beziehungen jeglicher Form und wie 
+diese durch Technik, Digitalität und Popkultur geprägt sind. Ihre Agenda ist klar feministisch.
+</p>
+
+<p>
+<strong>So erreichst du uns</strong><br>
+Per Mail: <a href="mailto:info@sternapau.de">info@sternapau.de</a>
+Instagram: <a href="https://www.instagram.com/sterna.pau/">@sterna.pau</a>
+</p>
+
+<p>
+<strong>Dankeschön</strong><br>
+Dieses Projekt war nur durch die Unterstützung vieler toller Menschen vor Ort und anderswo möglich. 
+Wir bedanken uns bei:
+</p>
+
+<p>
+<strong>"Von anderen Zeiten" wird gefördert durch</strong><br>
+</p>
+
+<p>
+<strong>Spenden</strong><br>
+Falls euch der Spaziergang gefallen hat, bitten wir euch, eine kleine Spende an ... zu schicken.
+</p>
+
+<p>
+<strong>Quellen 3D</strong><br>
+
+</p>
+
