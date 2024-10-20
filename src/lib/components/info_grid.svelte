4259ed83cc15f9146e6a9f2d30da7e1ed7f8a9b7
lislis <mail@lislis.de>
2024-10-20 16:26:02 +0200

    intro screen pt 2

---
+++ src/lib/components/info_grid.svelte
@@ -0,0 +1,63 @@
+<div class="gridlike">
+    <h2>
+        <span  class="a11y-hidden">Kopfhörer</span>
+        <img src="/icons/headphones.svg" width="40" height="40"></h2>
+        <div>
+            <p>
+                In der Karten-Ansicht kannst du sehen, wo du dich gerade befindest und an welchen Orten sich verschiedene Medien verstecken.<br>
+                <strong>Wichtig:</strong> Bitte gib der App die Berechtigung, auf deinen Standort zuzugreifen!
+            </p>
+        </div>
+
+    <h2>
+        <span  class="a11y-hidden">Karte</span>
+        <img src="/icons/map.svg" width="40" height="40"></h2>
+    <div>
+        <p>
+        In der Karten-Ansicht kannst du sehen, wo du dich gerade befindest und an welchen Orten sich verschiedene Medien verstecken.<br>
+        <strong>Wichtig:</strong> Bitte gib der App die Berechtigung, auf deinen Standort zuzugreifen!
+        </p>
+    </div>
+
+    <h2>
+        <span  class="a11y-hidden">GPS</span>
+        <img src="/icons/shoes.svg" width="40" height="40"></h2>
+    <div>
+        <p>
+        Im Zentrum der Karte siehst du deine Position auf der Karte.
+        </p>
+    </div>
+
+    <h2>
+        <span  class="a11y-hidden">3D</span>
+        <img src="/icons/3d.svg" width="40" height="40"></h2>
+    <div>
+        <p>
+        Das Rechteck steht für 3D-Elemente, die du durch deine Smartphone-Kamera sehen kannst.
+        Wenn du in der Nähe des markierten Ortes bist, schalte auf die Kamera-Ansicht um. <br>
+        <strong>Wichtig: </strong> Bitte gib der App die Berechtigung, auf deine Kamera zuzugreifen!
+        </p>
+    </div>
+
+    <h2>
+        <span  class="a11y-hidden">Audio</span>
+        <img src="/icons/audio.svg" width="40" height="40"></h2>
+    <div>
+        <p>
+        Das Lautsprecher-Symbol steht für Medien, die man hören kann.
+        Sie fangen automatisch an zu spielen, wenn du in einem Umkreis von 10 Meter des markierten Ortes bist.
+        </p>
+    </div>
+
+
+    <h2>
+        <span  class="a11y-hidden">Kamera</span>
+        <img src="/icons/camera.svg" width="40" height="40"></h2>
+    <div>
+        <p>
+        In der Kamera-Ansicht kannst du die Welt um dich herum durch die Kamera deines Smartphones sehen.
+        Hier haben sich einige Dinge versteckt, die du mit deinen eigenen Augen nicht wahrnehmen kannst.
+        </p>
+    </div>
+
+</div>
