e294c60dda32843a18fbf9ff4dcab2025f6226cf
lislis <mail@lislis.de>
2024-10-20 15:30:19 +0200

    meta page styles

--- src/routes/(meta)/+layout.svelte
+++ src/routes/(meta)/+layout.svelte
@@ -1,17 +1,21 @@
 <script>
+ import { page } from '$app/stores';
+ let path;
+ $: path = $page.url.pathname;
+
  let closeSrc = "/icons/close.png";
 </script>
 
 <div class="wrapper-meta">
-    <header>
-        <a href="/karte">zurück <img src={closeSrc} with="30" height="30" alt="schließen" /></a>
+    <header class="meta-back">
+        <a href="/karte">zurück <img src={closeSrc} with="36" height="36" alt="schließen" /></a>
     </header>
     <main>
         <div class="tabnav">
             <nav>
-                <a href="/anleitung">Anleitung</a>
-                <a href="/faq">FAQ</a>
-                <a href="/info">Info</a>
+                <a href="/anleitung" class={path === '/anleitung' ? 'active' : ''}>Anleitung</a>
+                <a href="/faq" class={path === '/faq' ? 'active' : ''}>FAQ</a>
+                <a href="/info" class={path === '/info' ? 'active' : ''}>Info</a>
             </nav>
         </div>
         <slot>
