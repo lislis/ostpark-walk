0fb6338926465e2d431127e95504a3b5c7bcdab8
jolandauhlig <54583851+jolandaschmidtt@users.noreply.github.com>
2024-10-27 10:01:48 +0100

    updated intro texts

--- src/lib/components/info_grid.svelte
+++ src/lib/components/info_grid.svelte
@@ -2,7 +2,7 @@
 
     <h2>
         <span  class="a11y-hidden">Karte</span>
-        <img src="/icons/map.svg" width="40" height="40" alt=""></h2>
+        <img src="/icons/home.svg" width="40" height="40" alt=""></h2>
     <div>
         <p>
         In der Karten-Ansicht hast du die Übersicht über den gesamten Ostfriedhof. Die verschiedenen Symbole wie Lautsprecher, Rechteck etc. markieren die Orte, an denen du Inhalte entdecken kannst. Wenn du nah genug bist, um auf sie zuzugreifen, beginnen sie zu hüpfen!<br>
@@ -20,22 +20,24 @@
     </div>
 
     <h2>
-        <span  class="a11y-hidden">3D</span>
-        <img src="/icons/3d.svg" width="40" height="40" alt=""></h2>
-    <div>
-        <p>
-        Das Rechteck steht für 3D-Elemente, die du durch deine Smartphone-Kamera sehen kannst.<br>
-        <strong>Wichtig: </strong> Bitte gib der App die Berechtigung, auf deine Kamera zuzugreifen!
-        </p>
-    </div>
-
-    <h2>
         <span  class="a11y-hidden">Audio</span>
         <img src="/icons/audio.svg" width="40" height="40" alt=""></h2>
     <div>
         <p>
         Das Lautsprecher-Symbol steht für Medien, die man hören kann.
-        Sie fangen automatisch an zu spielen, wenn du in einem Umkreis von 10 Meter des markierten Ortes bist.
+       Wenn du in einem Umkreis von 10 Meter des markierten Ortes bist, fängt die Markierung an zu hüpfen, ein Player erscheint und du kannst das Audio abspielen.
+       Wenn du auf "Transkript anzeigen" klickst, bekommst du das ganze als Text angezeigt. 
+
+        </p>
+    </div>
+
+    <h2>
+        <span  class="a11y-hidden">3D</span>
+        <img src="/icons/3d.svg" width="40" height="40" alt=""></h2>
+    <div>
+        <p>
+        Bei den Würfeln kannst du nicht nur etwas hören, sondern zusätzlich durch die Kamera deines Handys etwas sehen. Bitte klick dafür im Player auf "Kameraansicht".<br>
+        <strong>Wichtig: </strong> Bitte gib der App die Berechtigung, auf deine Kamera zuzugreifen!
         </p>
     </div>
 
@@ -43,6 +45,16 @@
     <h2>
         <span  class="a11y-hidden">Kamera</span>
         <img src="/icons/camera.svg" width="40" height="40" alt=""></h2>
+    <div>
+        <p>
+        In der Kamera-Ansicht kannst du die Welt um dich herum durch die Kamera deines Smartphones sehen.
+        Hier haben sich einige Dinge versteckt, die du mit deinen eigenen Augen nicht wahrnehmen kannst.
+        </p>
+    </div>
+
+    <h2>
+        <span  class="a11y-hidden">Fragen</span>
+        <img src="/icons/questions.svg" width="40" height="40" alt=""></h2>
     <div>
         <p>
         In der Kamera-Ansicht kannst du die Welt um dich herum durch die Kamera deines Smartphones sehen.
